840318c4b93fc182d134a9fcf5f4c51433cea589
Steve Cotner <stevecotner@gmail.com>
2019-06-14 23:29:18 -0700

    Initial Commit

---
+++ Sketch PaperTests/Sketch_PaperTests.swift
@@ -0,0 +1,34 @@
+//
+//  Sketch_PaperTests.swift
+//  Sketch PaperTests
+//
+//  Created by Stephen E. Cotner on 6/14/19.
+//  Copyright © 2019 Steve Cotner. All rights reserved.
+//
+
+import XCTest
+@testable import Sketch_Paper
+
+class Sketch_PaperTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
